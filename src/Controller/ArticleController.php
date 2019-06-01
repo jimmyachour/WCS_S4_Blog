0ca6a68aeb014b7fda9f82eeaedc3da95ac1cb31
Jimmy Achour <jimmyachour@hotmail.fr>
2019-06-01 11:22:25 +0200

    Crud

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,26 +1,34 @@
 <?php
 
-
 namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Route("/article", name="article_")
+ * @Route("/article")
  */
 class ArticleController extends AbstractController
 {
     /**
-     * Page create article
-     *
-     * @Route("/ajouter", name="add")
+     * @Route("/", name="article_index", methods={"GET"})
      */
-    public function addArticle(Request $request): Response
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="article_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
@@ -36,5 +44,51 @@ class ArticleController extends AbstractController
 
         }
         return $this->render('Blog/form.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/{id}", name="article_show", methods={"GET"})
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="article_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Article $article): Response
+    {
+        $form = $this->createForm(Article1Type::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('article_index', [
+                'id' => $article->getId(),
+            ]);
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="article_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Article $article): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('article_index');
     }
 }
